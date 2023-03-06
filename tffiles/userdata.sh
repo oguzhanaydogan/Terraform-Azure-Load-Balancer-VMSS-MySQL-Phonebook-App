01c2c91c31a56b90901c91513e2ada15ef914dd2
oguzhanaydogan <115114988+oguzhanaydogan@users.noreply.github.com>
2023-03-06 08:16:42 -0500

    Update userdata.sh

--- tffiles/userdata.sh
+++ tffiles/userdata.sh
@@ -9,8 +9,8 @@ pip3 install flask
 pip3 install flask_mysql
 
 # Alternatively, you can copy the file to the instance using a different method
-git clone https://github.com/oguzhanaydogan/terraform-lb-phonebook-app.git /home/clouduser/terraform-lb-phonebook-app/
-cd /home/clouduser/terraform-lb-phonebook-app/
+git clone https://github.com/oguzhanaydogan/Terraform-Azure-Load-Balancer-VMSS-MySQL-Phonebook-App.git /home/clouduser/Terraform-Azure-Load-Balancer-VMSS-MySQL-Phonebook-App/
+cd /home/clouduser/Terraform-Azure-Load-Balancer-VMSS-MySQL-Phonebook-App/
 
 # Start the Phonebook Application
 sudo python3 phonebook-app.py
